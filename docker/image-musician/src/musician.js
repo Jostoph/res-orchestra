44848cec56321bdbc2d81c5b1c375f5c33fe1939
Christoph Rueff <christoph.rueff@gmail.com>
2019-05-09 21:15:05 +0200

    Implement musician.js with lint fixes

--- docker/image-musician/src/musician.js
+++ docker/image-musician/src/musician.js
@@ -1,28 +1,44 @@
-let protocol = require('./musician-protocol');
+const dgram = require('dgram');
+const protocol = require('./musician-protocol');
 
-let dgram = require('dgram');
+const socket = dgram.createSocket('udp4');
 
-let socket = dgram.createSocket('udp4');
+const instruments = new Map();
+instruments.set('piano', 'ti-ta-ti');
+instruments.set('trumpet', 'pouet');
+instruments.set('flute', 'trulu');
+instruments.set('violin', 'gzi-gzi');
+instruments.set('drum', 'boum-boum');
 
 class Musician {
-
-    // TODO make class instrument ?
-    constructor(instrument) {
-        this.instrument = instrument;
+  constructor(instrument) {
+    if (!instruments.has(instrument)) {
+      throw Error(`error : invalid instrument : ${instrument}`);
     }
 
-    play() {
+    this.instrument = instrument;
+    setInterval(this.play.bind(this), 1000);
+  }
 
-        let soudPatern = "undefined"
+  play() {
+    const music = {
+      sound: instruments.get(this.instrument),
+    };
+    const payload = JSON.stringify(music);
 
-
-        // check in map(instr-->sound)
-        // send string ? json ?
-    }
-
-    // set interval
+    const message = Buffer.from(payload);
+    socket.send(message, 0, message.length, protocol.PROTOCOL_PORT,
+      protocol.PROTOCOL_MULTICAST_ADDRESS,
+      (err, bytes) => {
+        console.log(`Sending payload: ${payload} via port ${socket.address().port}`);
+      });
+  }
 }
 
-let instrument = process.argv[2];
+const instrument = process.argv[2];
 
-let musician = new Musician(instrument);
+try {
+  Musician(instrument);
+} catch (error) {
+  console.log(error);
+}
