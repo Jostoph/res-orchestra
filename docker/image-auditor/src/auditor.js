caab2733f421e1661a873b8247eb5bd8ca9b4bd2
Christoph Rueff <christoph.rueff@gmail.com>
2019-05-10 15:09:30 +0200

    Add function with 1000ms interval to remove inactive musicians

--- docker/image-auditor/src/auditor.js
+++ docker/image-auditor/src/auditor.js
@@ -26,6 +26,17 @@ udpSocket.bind(protocol.PROTOCOL_UDP_PORT, () => {
   udpSocket.addMembership(protocol.PROTOCOL_MULTICAST_ADDRESS);
 });
 
+function removeInactiveMusicians() {
+  let currentDate = new Date();
+  for(let entry of musicians.entries()) {
+    if (currentDate.getTime() - entry[1].activeSince.getTime() > 5000) {
+      musicians.delete(entry[0]);
+    }
+  }
+}
+
+setInterval(removeInactiveMusicians, 1000);
+
 udpSocket.on('message', (msg, source) => {
   const obj = JSON.parse(msg);
   const { id, sound } = obj;
